ab6d2daf613d814297a24c0a63b326c5dca16682
Gabriela Singulani <gabriela.singulani@ice.ufjf.br>
2025-08-22 11:25:54 -0300

    Adiciona parâmetro direcionado

--- trabalho2/main.cpp
+++ trabalho2/main.cpp
@@ -3,28 +3,36 @@
 #include <chrono>
 
 int main (int argc, char *argv[]) {
-    if (argc < 2) { 
-        std::cerr << "Erro: Faltando o nome do arquivo de entrada." << std::endl;
-        std::cerr << "Uso: " << argv[0] << " <caminho_para_o_arquivo>" << std::endl;
+    if (argc < 3) { 
+        std::cerr << "Erro: Faltando parâmetros." << std::endl;
+        std::cerr << "Uso: " << argv[0] << " <caminho_para_o_arquivo> <direcionado?>" << std::endl;
         return 1; 
     }
     std::string nome_arquivo = argv[1];
+    std::string direcionado = argv[2];
+
     std::ifstream instance_file(nome_arquivo);
+    if(direcionado != "true" && direcionado != "false") {
+        std::cerr << "Erro: O segundo parâmetro deve ser 'true' ou 'false'." << std::endl;
+        return 1;
+    }
+
     //verifica se o arquivo foi aberto corretamente
     if (!instance_file.is_open()) {
         std::cerr << "Nao foi possivel abrir o arquivo " << nome_arquivo << std::endl;
         return 1;
     }
-    Graph graph(instance_file, true);
+
+    Graph graph(instance_file, direcionado == "true");
     auto start = std::chrono::high_resolution_clock::now();
     //verifica se o grafo orientado é fracamente conexo
     bool eh_conexo = graph.verifica_conectividade_fraca();
     auto end = std::chrono::high_resolution_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
     if (eh_conexo) {
-        std::cout << "O grafo orientando eh conexo." << std::endl;
+        std::cout << "O grafo " << ((direcionado == "true") ? "" : "não ") << "orientado eh conexo." << std::endl;
     } else {
-        std::cout << "O grafo orientado eh desconexo." << std::endl;
+        std::cout << "O grafo " << ((direcionado == "true") ? "" : "não ") << "orientado eh desconexo." << std::endl;
     }
     std::cout << "Tempo de execucao: " << elapsed.count() << " nanosegundos." << std::endl;
     return 0;
