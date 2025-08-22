464428771c97cfdb792edad4696569d029caa60b
Gabriela Singulani <gabriela.singulani@ice.ufjf.br>
2025-08-22 11:12:39 -0300

    Tempo de execução da função de conetividade

--- trabalho2/main.cpp
+++ trabalho2/main.cpp
@@ -1,5 +1,6 @@
 #include "include/Graph.hpp"
 #include "include/defines.hpp"
+#include <chrono>
 
 int main (int argc, char *argv[]) {
     if (argc < 2) { 
@@ -14,13 +15,17 @@ int main (int argc, char *argv[]) {
         std::cerr << "Nao foi possivel abrir o arquivo " << nome_arquivo << std::endl;
         return 1;
     }
-    Graph graph(instance_file, true, false, false);
+    Graph graph(instance_file, true);
+    auto start = std::chrono::high_resolution_clock::now();
     //verifica se o grafo orientado é fracamente conexo
     bool eh_conexo = graph.verifica_conectividade_fraca();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
     if (eh_conexo) {
         std::cout << "O grafo orientando eh conexo." << std::endl;
     } else {
         std::cout << "O grafo orientado eh desconexo." << std::endl;
     }
+    std::cout << "Tempo de execucao: " << elapsed.count() << " nanosegundos." << std::endl;
     return 0;
 }
